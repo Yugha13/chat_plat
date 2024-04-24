0e593f9d4bcb539531866e517a548ac458172002
S-Axhwin <ashwin.sathiyamoorthy.d@gmail.com>
2024-04-25 02:19:36 +0530

    changed port again

--- src/store/Slice/userSlice.tsx
+++ src/store/Slice/userSlice.tsx
@@ -5,7 +5,7 @@ export const fetchUser = createAsyncThunk("fetchUserToken", async(payload:any) =
     const { username, password, type } = payload
     console.log(username, password, type);
     
-    const data:any = axios.post(`http://192.168.179.238:5001/api/${type}`, {username, password})
+    const data:any = axios.post(`http://10.35.42.162:5001/api/${type}`, {username, password})
     .then((res:any) => {
         console.log(res);
         localStorage.setItem("token", res.data.token)
@@ -13,7 +13,7 @@ export const fetchUser = createAsyncThunk("fetchUserToken", async(payload:any) =
     })
     return data
 })
-
+//10.35.42.162
 const inti = {
     username: "",
     isloggedIn: false,
